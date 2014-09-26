90ace0c026cc3c71f65e8772154943705fae95bc
Aguacero <demdxx@gmail.com>
2014-09-26 16:36:16 +0400

    Add license file

--- NSThread+Blocks.m
+++ NSThread+Blocks.m
@@ -13,61 +13,58 @@
 #import "NSThread+Blocks.h"
 
 #ifndef __has_feature
-#   define __has_feature(x) 0
+  #define __has_feature(x) 0
 #endif
 
 #if __has_feature(objc_arc)
-#   define TH_OBJECT_AUTORELEASE(obj) obj
+  #define TH_OBJECT_AUTORELEASE(obj) obj
 #else
-#   define TH_OBJECT_AUTORELEASE(obj) [obj autorelease]
+  #define TH_OBJECT_AUTORELEASE(obj) [obj autorelease]
 #endif
 
 @implementation NSThread (Blocks)
 
 - (void)performBlock:(void(^)())block
 {
-	if ([[NSThread currentThread] isEqual:self])
-    {
-		block();
-    }
-	else
-    {
-		[self performBlock:block waitUntilDone:NO];
-    }
+  if ([[NSThread currentThread] isEqual:self]) {
+    block();
+  } else {
+    [self performBlock:block waitUntilDone:NO];
+  }
 }
 
 - (void)performBlock:(void(^)())block waitUntilDone:(BOOL)wait
 {
-    [NSThread performSelector:@selector(ng_runBlock:)
-                     onThread:self
-                   withObject:TH_OBJECT_AUTORELEASE([block copy])
-                waitUntilDone:wait];
+  [NSThread performSelector:@selector(ng_runBlock:)
+                   onThread:self
+                 withObject:TH_OBJECT_AUTORELEASE([block copy])
+              waitUntilDone:wait];
 }
 
 + (void)ng_runBlock:(void(^)())block
 {
-	block();
+  block();
 }
 
 + (void)performBlockInBackground:(void(^)())block
 {
-	#ifdef __IPHONE_4_0
-	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), block);
-	#else
-	[NSThread performSelectorInBackground:@selector(ng_runBlock:)
-	                           withObject:TH_OBJECT_AUTORELEASE([block copy])];
-	#endif
+#ifdef __IPHONE_4_0
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0), block);
+#else
+  [NSThread performSelectorInBackground:@selector(ng_runBlock:)
+                             withObject:TH_OBJECT_AUTORELEASE([block copy])];
+#endif
 }
 
 + (void)performBlockOnMainThread:(void(^)())block
 {
-	#ifdef __IPHONE_4_0
-	dispatch_async(dispatch_get_main_queue(), block);
-	#else
-	[NSThread performSelectorOnMainThread:@selector(ng_runBlock:)
+#ifdef __IPHONE_4_0
+  dispatch_async(dispatch_get_main_queue(), block);
+#else
+  [NSThread performSelectorOnMainThread:@selector(ng_runBlock:)
 	                           withObject:TH_OBJECT_AUTORELEASE([block copy])
                             waitUntilDone:NO];
-	#endif
+#endif
 }
 
 @end
--- NSThreadBlocks.podspec
+++ NSThreadBlocks.podspec
@@ -3,6 +3,7 @@ Pod::Spec.new do |s|
   s.author          = { "Dmitry Ponomarev" => "demdxx@gmail.com" }
   s.version         = '0.0.1'
   s.license         = 'MIT'
+  s.summary         = 'Extension of the NSThread.'
   s.homepage        = 'https://github.com/demdxx/NSThreadBlocks'
   s.source          = {:git => 'https://github.com/demdxx/NSThreadBlocks.git', :tag => 'v0.0.1'}
 
